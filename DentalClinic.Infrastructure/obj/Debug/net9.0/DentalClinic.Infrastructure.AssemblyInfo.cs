26a71e6dda42b4c801d48d63f5f072b989f8f3ed
tthiennes-eng <tthiennes@gmail.com>
2026-07-19 22:39:44 -0300

    5

--- DentalClinic.Infrastructure/obj/Debug/net9.0/DentalClinic.Infrastructure.AssemblyInfo.cs
+++ DentalClinic.Infrastructure/obj/Debug/net9.0/DentalClinic.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DentalClinic.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47b0086fd45829b1b7c332a526a743361ae48471")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8a961f671782d42dbed4e0af7b37ca2fb3b241")]
 [assembly: System.Reflection.AssemblyProductAttribute("DentalClinic.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DentalClinic.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
