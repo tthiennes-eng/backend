20d40648cd464ea5e3e40539068826b94dfa5666
tthiennes-eng <tthiennes@gmail.com>
2026-07-20 03:09:13 -0300

    7

--- DentalClinic.Api/obj/Debug/net9.0/DentalClinic.Api.AssemblyInfo.cs
+++ DentalClinic.Api/obj/Debug/net9.0/DentalClinic.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DentalClinic.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0bd5bddeb28496d6170cd40c427988d8812bc9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fdfa16c7955fae141e9f227c4ed89960532547a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DentalClinic.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DentalClinic.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
