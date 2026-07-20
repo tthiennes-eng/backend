720777d313954e44323cb69f96c3124cea47ee77
tthiennes-eng <tthiennes@gmail.com>
2026-07-20 01:55:14 -0300

    7

--- DentalClinic.Core/obj/Debug/net9.0/DentalClinic.Core.AssemblyInfo.cs
+++ DentalClinic.Core/obj/Debug/net9.0/DentalClinic.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DentalClinic.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27f1348a8b5d40176c3525a226f0b090b70ea55b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+534d27632d7df26efd843dfe2a5f3b221fdf60e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DentalClinic.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DentalClinic.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
